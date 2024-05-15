624635107cc61e153990b8a01dbef43a64f2dc37
kefer16 <morafi.1999.16@gmail.com>
2024-05-14 21:30:10 -0500

    se agrega metadata a head html

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,8 +3,17 @@ import "./globals.css";
 import { ThemeProvider } from "@/components/theme-provider";
 
 export const metadata: Metadata = {
-   title: "Portafolio - Kevin Morales Figueroa",
-   description: "Kevin Morales Figueroa",
+   metadataBase: new URL("https://kefer.dev"),
+   title: "Portafolio | Kevin Morales Figueroa",
+   description: "Portafolio Web",
+   openGraph: {
+      title: "Portafolio | Kevin Morales Figueroa",
+      description: "Portafolio Web",
+      type: "website",
+      url: "https://kefer.dev",
+      images: "/login.jpeg",
+      siteName: "@kefer.dev",
+   },
 };
 
 export default function RootLayout({
@@ -14,6 +23,10 @@ export default function RootLayout({
 }>) {
    return (
       <html className="scroll-smooth" lang="en">
+         {/* <head>
+            <link rel="canonical" href="https://kefer.dev" />
+            <meta property="og:image" content="https:///kefer.dev/login.jpeg" />
+         </head> */}
          <body>
             <ThemeProvider
                attribute="class"
