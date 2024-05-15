1408d56a7f861c6392bbf68b3f42d225b5db8c12
kefer16 <morafi.1999.16@gmail.com>
2024-05-14 21:53:05 -0500

    se agrega metadata de twitter

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,16 +3,21 @@ import "./globals.css";
 import { ThemeProvider } from "@/components/theme-provider";
 
 export const metadata: Metadata = {
-   metadataBase: new URL("https://kefer.dev"),
+   metadataBase: new URL("https://kefer.dev/"),
    title: "Portafolio - Kevin Morales Figueroa",
    description: "Portafolio Web",
    openGraph: {
       title: "Portafolio - Kevin Morales Figueroa",
       description: "Portafolio Web",
       type: "website",
-      url: "https://kefer.dev",
-      images: "/login.jpeg",
-      siteName: "@kefer.dev",
+      images: "login.jpeg",
+      siteName: "kefer",
+   },
+   twitter: {
+      card: "summary_large_image",
+      title: "Portafolio - Kevin Morales Figueroa",
+      description: "Portafolio Web",
+      images: "login.jpeg",
    },
 };
 
