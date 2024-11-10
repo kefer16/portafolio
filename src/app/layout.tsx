0570903336c586798a8482672faf631d40c0cf4a
Kevin Morales <66371768+kefer16@users.noreply.github.com>
2024-11-10 12:25:37 -0500

    formateo en tabs

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -39,8 +39,8 @@ export default function RootLayout({
             <ThemeProvider
                attribute="class"
                defaultTheme="dark"
-               // enableSystem
-               // disableTransitionOnChange
+            // enableSystem
+            // disableTransitionOnChange
             >
                {children}
             </ThemeProvider>
